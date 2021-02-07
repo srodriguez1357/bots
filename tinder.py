5a2d926e0f93f9bc784fcfac128ef37f2f59b2ec
srodriguez1357 <chavarrieta.2000@gmail.com>
2021-02-06 18:23:20 -0600

    Update tinder.py

--- tinder.py
+++ tinder.py
@@ -23,10 +23,10 @@ class TinderBot():
         self.driver.switch_to.window(self.driver.window_handles[1])
 
         email_in = self.driver.find_element_by_xpath('//*[@id="email"]')
-        email_in.send_keys('chemsapeluchadito@gmail.com')
+        email_in.send_keys('')
 
         pw_in = self.driver.find_element_by_xpath('//*[@id="pass"]')
-        pw_in.send_keys('Yo123456789')
+        pw_in.send_keys('')
 
         login_btn = self.driver.find_element_by_xpath('//*[@id="u_0_0"]')
         login_btn.click()
